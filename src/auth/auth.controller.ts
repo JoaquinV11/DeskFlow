9b3a8bf910cfe31ca356a6f4ab7f2ddfd581d047
JoaquinV11 <joaquinvv220241@gmail.com>
2026-03-16 16:30:17 -0300

    Agrego roles USER, AGENT y ADMIN

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
+import { Roles } from './decorators/roles.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +18,27 @@ export class AuthController {
   @Get('me')
   me(@Req() req: any) {
     return req.user;
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
+  @Get('test/admin')
+  adminOnly(@Req() req: any) {
+    return {
+      ok: true,
+      message: 'Ruta solo ADMIN',
+      user: req.user,
+    };
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'AGENT')
+  @Get('test/support')
+  supportOnly(@Req() req: any) {
+    return {
+      ok: true,
+      message: 'Ruta ADMIN o AGENT',
+      user: req.user,
+    };
   }
 }
